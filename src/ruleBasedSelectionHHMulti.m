c4f6e7952d31d816b1978baf729a5a4aa0377a82
iamaya2 <iamaya2@gmail.com>
2022-04-12 17:39:11 -0500

    Update ruleBasedSelectionHHMulti.m
    Work on basic functionality

--- src/ruleBasedSelectionHHMulti.m
+++ src/ruleBasedSelectionHHMulti.m
@@ -7,8 +7,12 @@ classdef ruleBasedSelectionHHMulti < ruleBasedSelectionHH
 %
 %   See also: ruleBasedSelectionHH
     properties
-        nbLayers = 1; % Number of layers for the HH. Defaults to 1.
-        innerHHs = NaN; % Array of internal multi-layered HHs
+        % Number of layers for the HH. Defaults to 1.
+        nbLayers = 1; 
+        
+        % Array of internal multi-layered HHs.
+        %   Defaults to a HH with a single rule (i.e. a heuristic).
+        innerHHs = NaN; 
     end
     
     methods
@@ -19,21 +23,17 @@ classdef ruleBasedSelectionHHMulti < ruleBasedSelectionHH
             callErrorCode(0); % WIP
             
             obj = obj@ruleBasedSelectionHH(varargin{:}); % this must be tested. Should create basic HH with props
-            
-%             defaultFeatures = 1:length(obj.availableFeatures);
-%             defaultSolvers  = 1:length(obj.availableSolvers);
-            defaultNbLayers = 1;
-%             defaultNbRules  = 2;            
-%             defaultProblemHandle = @JSSP;                        
-            
+            defaultNbLayers = 2;       
+            defaultInnerHHs = ruleBasedSelectionHH(struct('nbRules',1)); 
             if nargin >=1 
                 props = varargin{1};                
-                if isfield(props,'nbLayers'), obj.nbLayers = props.nbLayers; end
+                if ~isfield(props,'nbLayers'), props.nbLayers = defaultNbLayers; end
+                if ~isfield(props,'innerHHs'), props.innerHHs = defaultInnerHHs; end
 %                 if ~isfield(props,'nbRules'), props.nbRules = defaultNbRules; end                
 %                 if ~isfield(props,'selectedFeatures'), props.selectedFeatures = defaultFeatures; end
 %                 if ~isfield(props,'selectedSolvers'), props.selectedSolvers = defaultSolvers; end                
 %                 if ~isfield(props,'targetProblemHandle'), props.targetProblemHandle = defaultProblemHandle; end
-%                 obj.initializeParameters(props);
+                obj.initializeParameters(props);
             end
             obj.initializeModel(obj.nbRules);
         end
@@ -43,10 +43,11 @@ classdef ruleBasedSelectionHHMulti < ruleBasedSelectionHH
         % ----- ---------------------------------------------------- -----
         function initializeParameters(obj, props)
             obj.nbLayers = props.nbLayers;
-            obj.nbRules = props.nbRules;            
-            obj.assignProblem(props.targetProblemHandle);
-            obj.assignFeatures(props.selectedFeatures);
-            obj.assignSolvers(props.selectedSolvers);
+            obj.innerHHs = props.innerHHs;
+%             obj.nbRules = props.nbRules;            
+%             obj.assignProblem(props.targetProblemHandle);
+%             obj.assignFeatures(props.selectedFeatures);
+%             obj.assignSolvers(props.selectedSolvers);
         end
         
         % ----- ---------------------------------------------------- -----
